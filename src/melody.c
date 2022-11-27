1a3075712975d72137af176ced56154fec9d2c31
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-11-27 12:25:06 +0100

    move melody definition to separate file

---
+++ src/melody.c
@@ -0,0 +1,67 @@
+#define TRACK_BPM 138
+
+#pragma region Helpers
+#include "music.h"
+
+#define NOTE(octave, note, duration) {(octave), (note), (duration * TICKS_PER_SIXTEENTH)}
+
+#define DURATION_WHOLE 16
+#define DURATION_HALF 8
+#define DURATION_QUARTER 4
+
+#define TREBLE(offset) (OCTAVE_3 + (offset))
+#define BASS(offset) (OCTAVE_1 + (offset))
+
+#define REST(duration) NOTE(OCTAVE_1, NOTE_NONE, (duration))
+#define WNOTE(octave, note) NOTE((octave), (note), DURATION_WHOLE)
+#define HNOTE(octave, note) NOTE((octave), (note), DURATION_HALF)
+#define QNOTE(octave, note) NOTE((octave), (note), DURATION_QUARTER)
+#pragma endregion
+
+const Note MELODY[] = {
+    REST(DURATION_WHOLE * 2),
+
+    // 1
+    HNOTE(BASS(0), NOTE_C),
+    QNOTE(BASS(0), NOTE_C),
+    QNOTE(BASS(1), NOTE_E),
+
+    REST(DURATION_QUARTER),
+
+    QNOTE(BASS(1), NOTE_E),
+    QNOTE(BASS(1), NOTE_D),
+    QNOTE(BASS(1), NOTE_E),
+
+    HNOTE(BASS(0), NOTE_C),
+    QNOTE(BASS(0), NOTE_C),
+    QNOTE(BASS(1), NOTE_E),
+
+    REST(DURATION_QUARTER),
+
+    QNOTE(BASS(1), NOTE_E),
+    QNOTE(BASS(1), NOTE_D),
+    QNOTE(BASS(1), NOTE_E),
+
+    // 2
+    HNOTE(BASS(0), NOTE_C),
+    QNOTE(BASS(0), NOTE_C),
+    QNOTE(BASS(1), NOTE_E),
+
+    REST(DURATION_QUARTER),
+
+    QNOTE(BASS(1), NOTE_E),
+    QNOTE(BASS(1), NOTE_D),
+    QNOTE(BASS(1), NOTE_E),
+
+    HNOTE(TREBLE(0), NOTE_G),
+    QNOTE(TREBLE(0), NOTE_E),
+    QNOTE(TREBLE(0), NOTE_G),
+    HNOTE(TREBLE(0), NOTE_A),
+    QNOTE(TREBLE(0), NOTE_E),
+    QNOTE(TREBLE(0), NOTE_A),
+    
+
+    // END
+    REST(DURATION_WHOLE * 2),
+    {END_OCTAVE, END_NOTE, END_DURATION_STOP},
+};
--- src/music.h
+++ src/music.h
@@ -4,13 +4,18 @@
 #include "lib/sound.h"
 #include "lib/timer.h"
 
-#include "lib/screen.h"
-#include "lib/font.h"
-
 #define END_OCTAVE 0xff
 #define END_NOTE 0xff
 #define END_DURATION_STOP 0x00
 #define END_DURATION_LOOP 0xff
+
+#ifndef TRACK_BPM
+#define TRACK_BPM 100
+#endif
+
+#define TRACK_BPS (TRACK_BPM / 60.0)
+#define TICKS_PER_BEAT (TIMER_TPS / TRACK_BPS)
+#define TICKS_PER_SIXTEENTH (TICKS_PER_BEAT / 16.0)
 
 typedef struct Note
 {
@@ -26,74 +31,9 @@ typedef struct ActiveNote
     u32 remainingTicks;
 } ActiveNote;
 
+extern const Note MELODY[];
+
 void music_init();
 
 void music_tick(u32 ticks);
-
-#define TRACK_BPM 150
-#define TRACK_BPS (TRACK_BPM / 60.0)
-#define TICKS_PER_BEAT (TIMER_TPS / TRACK_BPS)
-#define TICKS_PER_SIXTEENTH (TICKS_PER_BEAT / 16.0)
-#define NOTE(octave, note, duration)                       \
-    {                                                      \
-        (octave), (note), (duration * TICKS_PER_SIXTEENTH) \
-    }
-
-static const struct Note MELODY[] = {
-    NOTE(OCTAVE_5, NOTE_E, 16),
-    NOTE(OCTAVE_4, NOTE_B, 8),
-    NOTE(OCTAVE_5, NOTE_C, 8),
-    NOTE(OCTAVE_5, NOTE_D, 16),
-
-    NOTE(OCTAVE_5, NOTE_C, 8),
-    NOTE(OCTAVE_4, NOTE_B, 8),
-    NOTE(OCTAVE_4, NOTE_A, 16),
-
-    NOTE(OCTAVE_4, NOTE_A, 8),
-    NOTE(OCTAVE_5, NOTE_C, 8),
-    NOTE(OCTAVE_5, NOTE_E, 16),
-
-    NOTE(OCTAVE_5, NOTE_D, 8),
-    NOTE(OCTAVE_5, NOTE_C, 8),
-    NOTE(OCTAVE_4, NOTE_B, 16),
-
-    NOTE(OCTAVE_4, NOTE_B, 8),
-    NOTE(OCTAVE_5, NOTE_C, 8),
-    NOTE(OCTAVE_5, NOTE_D, 16),
-    NOTE(OCTAVE_5, NOTE_E, 16),
-    NOTE(OCTAVE_5, NOTE_C, 16),
-    NOTE(OCTAVE_4, NOTE_A, 16),
-    NOTE(OCTAVE_4, NOTE_A, 16),
-    NOTE(OCTAVE_4, NOTE_NONE, 24),
-
-    NOTE(OCTAVE_5, NOTE_D, 16),
-    NOTE(OCTAVE_5, NOTE_F, 8),
-    NOTE(OCTAVE_5, NOTE_A, 8),
-    NOTE(OCTAVE_5, NOTE_A, 4),
-    NOTE(OCTAVE_5, NOTE_A, 4),
-    NOTE(OCTAVE_5, NOTE_G, 8),
-    NOTE(OCTAVE_5, NOTE_F, 8),
-    NOTE(OCTAVE_5, NOTE_E, 16),
-    NOTE(OCTAVE_5, NOTE_NONE, 8),
-
-    NOTE(OCTAVE_5, NOTE_C, 8),
-    NOTE(OCTAVE_5, NOTE_E, 8),
-    NOTE(OCTAVE_5, NOTE_E, 4),
-    NOTE(OCTAVE_5, NOTE_E, 4),
-    NOTE(OCTAVE_5, NOTE_D, 8),
-    NOTE(OCTAVE_5, NOTE_C, 8),
-    NOTE(OCTAVE_4, NOTE_B, 16),
-
-    NOTE(OCTAVE_4, NOTE_B, 8),
-    NOTE(OCTAVE_5, NOTE_C, 8),
-    NOTE(OCTAVE_5, NOTE_D, 16),
-    NOTE(OCTAVE_5, NOTE_E, 16),
-    NOTE(OCTAVE_5, NOTE_C, 16),
-    NOTE(OCTAVE_4, NOTE_A, 16),
-    NOTE(OCTAVE_4, NOTE_A, 16),
-    NOTE(OCTAVE_4, NOTE_NONE, 16),
-
-    // END
-    {END_OCTAVE, END_NOTE, END_DURATION_LOOP},
-};
 #endif
